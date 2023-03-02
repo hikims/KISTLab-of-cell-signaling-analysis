355f4cf6846c7b8fa8903aab077eb7077e054392
hikims <hikims@kist.re.kr>
2023-03-02 14:16:58 +0900

    execute file

---
+++ exec.sh
@@ -0,0 +1,9 @@
+g++ -std=c++11 main.cpp initial_data.cpp Boolean.cpp functions.cpp -lm
+#cat kulist.txt | xargs -L 1 ./a.out
+#cat levellist.txt | xargs -L 1 ./a.out
+#cat kudlist.txt | xargs -L 1 ./a.out
+#cat kdsiglist.txt | xargs -L 1 ./a.out
+./a.out 
+
+
+
